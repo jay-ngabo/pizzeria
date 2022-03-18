0871776edc48efefd638537de43148d7405522b1
jacqueline ngabo <ngabo.jay@gmail.com>
2022-03-19 00:16:38 +0300

    add javascript changes

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // $(".col-lg-4").click(function() 
+        
+    
+    })
+    $('.col-lg-4').hover(function(){
+        $(this).find('.overlay').toggle();
+    
+    }
+        )
