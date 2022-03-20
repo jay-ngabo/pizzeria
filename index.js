04a7bfe92907718038b2bd80fdc948fa917af54a
jacqueline ngabo <ngabo.jay@gmail.com>
2022-03-20 18:19:02 +0300

    add changes to javascript

--- index.js
+++ index.js
@@ -1,10 +1,99 @@
+var price, toppingsPrice;
+let total = 0;
+function getPizza(flavour,size,toppings,crust,total){
+    this.flavour = flavour;
+    this.size = size;
+    this.toppings = toppings;
+    this.crust = crust
+    this.total = total;
+
+};
 $(document).ready(function() {
-    // $(".col-lg-4").click(function() 
-        
-    
-    })
     $('.col-lg-4').hover(function(){
         $(this).find('.overlay').toggle();
+    })
+    $('.submit-button').click(function(event){
+        let pflavour = $('#pflavour option:selected').val();
+        let pSize = $('#size option:selected').val();
+        let ptoppings = $('#toppings option:selected').val();
+        let pCrust = $('#crust option:selected').val();
+
+        switch(pSize){
+            case "0":
+            price = 0;
+            break;
+            
+            
+            case 'large':
+            price = 1000;
+            console.log(price);
+            break;
+
+
+            case 'medium':
+            price = 800;
+            console.log(price);
+            break;
+
+            
+            case 'regular':
+            price = 500;
+            console.log(price);
+            break;
+
+
+
+        }
+
+        switch(ptoppings){
+            case "O":
+                price = 0;
+            break;
+
+            case "yes":
+                price = 50;
+                console.log(price)
+            break;
+
+            case "no":
+                price = 50;
+                console.log(price)
+            break;
+
+        }
+
+        switch(ptoppings){
+            case "O":
+                price = 0;
+            break;
+
+            case "yes":
+                price = 50;
+                console.log(price)
+            break;
+
+            case "no":
+                price = 50;
+                console.log(price)
+            break;
+
+        }
+        if ((pSize == "0") && (ptoppings == "0")){
+            console.log('nothing selected');
+
+
+        }
+
+
+
+        event.preventDefault();
+
+
+
+    });
     
-    }
-        )
+    
+})
+
+    
+   
